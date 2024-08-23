7669e963a52ca52b87ed4a9cf5dd595ad5f9fcdd
SuriRoth <roth0725@gmail.com>
2024-08-22 22:04:47 -0400

    created the login/register form and added the authorization in flask bcrypt.

--- TaskPals_Backend/models.py
+++ TaskPals_Backend/models.py
@@ -19,7 +19,14 @@ class Customer(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     phone = db.Column(db.String(20), nullable=True)
+    password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    def set_password(self, password):
+            self.password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def check_password(self, password):
+            return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))    
 
 class Service(db.Model):
     __tablename__ = 'services'
