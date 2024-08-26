b79c13fcea0637c078c8381e8199fdb4133cbd77
rikki abramson <rikkiabramson@gmail.com>
2024-08-26 12:45:48 -0400

    added another file

--- TaskPals_Frontend/src/FormProvider.js
+++ TaskPals_Frontend/src/FormProvider.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
-import WebcamCapture from './WebcamCapture';
+import WebcamCapture from './WebcamCapture.js';
 
 const SignupForm = () => {
   const [formData, setFormData] = useState({
