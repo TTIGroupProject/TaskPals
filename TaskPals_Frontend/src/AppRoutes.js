06e87b127e893c7d9cf0c6aafaea7bacf154804e
rikki abramson <rikkiabramson@gmail.com>
2024-08-26 12:02:44 -0400

    changed made

--- TaskPals_Frontend/src/AppRoutes.js
+++ TaskPals_Frontend/src/AppRoutes.js
@@ -1,13 +1,18 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
-import AboutUs from "./AboutUs";
-import FormProvider from "./FormProvider";
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomePage from './HomePage';
+import Legal from './Legal';
+import DoNotSellMyInfoPage from "./DoNotSellMyInfo";
+import Terms from "./TermsAndPrivacy";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        {/* { <Route path="/service/:serviceId" element={<ServicePage/>}/> } */}
+        <Route path = '/' element={<HomePage/>}/>
+        <Route path="/TermsAndPrivacy" element={<Terms/>} />
+        <Route path="/DoNotSellMyInfo" element={<DoNotSellMyInfoPage/>} />
+        <Route path="/Legal" element={<Legal/>} />
         <Route path="/" element={<AboutUs/>}/>
         <Route path="/apply" element={<FormProvider/>}/>
       </Routes>
--- TaskPals_Frontend/src/index.js
+++ TaskPals_Frontend/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import AppRoutes from './AppRoutes';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
