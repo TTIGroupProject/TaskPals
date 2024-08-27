9910aa702a62aba2faabb5a26307a607ef4acdc5
SuriRoth <roth0725@gmail.com>
2024-08-27 13:49:39 -0400

    updated the backend

--- TaskPals_Backend/venv/Scripts/activate.bat
+++ TaskPals_Backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\roth0\OneDrive\TTIProgrammingCourse\Ferguson\TaskPals\TaskPals_Backend\venv
+set VIRTUAL_ENV=C:\Users\roth0\OneDrive\TTIProgrammingCourse\TaskPals\TaskPals_Backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
